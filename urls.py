a5970b697eb767f0b6e78157f2f0a5d63626fa70
Arsensh2004 <a.shaker@aues.kz>
2025-05-02 22:32:07 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,21 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
+# URL для переключения языка
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
-    path('quiz/', views.quiz_start, name='quiz_start'),
-    path('quiz/run/', views.quiz_run, name='quiz_run'),
-    path('quiz/result/', views.quiz_result, name='quiz_result'),
-path('register/', views.register, name='register'),
-path('login/', views.user_login, name='login'),
-path('logout/', views.user_logout, name='logout'),
-path('profile/', views.profile, name='profile'),
-path('about/', views.about, name='about'),
-path('literature/', views.literature, name='literature'),  # 🔹 литература
-path('videos/', views.videos, name='videos'),              # 🔹 видео
-path('articles/', views.articles, name='articles'),
-
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+# Основные маршруты с поддержкой i18n
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('ethics.urls')),
+    prefix_default_language=False
+)
+
+# Подключение статики (если нужно для продакшена)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
